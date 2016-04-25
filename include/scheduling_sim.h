031c0bc1c7322a7a44963ad4732b6afef40890bb
ChenZewei <czwking1991@gmail.com>
2016-04-25 11:31:45 +0800

    add scheduling_sim head file and source file

---
+++ include/scheduling_sim.h
@@ -0,0 +1,10 @@
+#ifndef SCHED_SIM_H
+#define SCHED_SIM_H
+
+#include "tasks.h"
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+
+#endif
---
+++ src/scheduling_sim.cpp
@@ -0,0 +1,1 @@
+#include "../include/scheduling_sim.h"
