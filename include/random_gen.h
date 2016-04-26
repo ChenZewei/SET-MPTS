17dd98238b106e9b6ed9f8a1471331dcf1b98328
ChenZewei <czwking1991@gmail.com>
2016-04-26 07:11:29 -0700

    implement random_gen.h

---
+++ include/random_gen.h
@@ -0,0 +1,27 @@
+#ifndef RANDOM_GEN_H
+#define RAMDOM_GEN_H
+
+#include <random>
+#include "types.h"
+using namespace std;
+
+default_random_engine generator(time(NULL));
+
+double exponential_gen(double lambda)
+{
+	exponential_distribution<double> distribution(lambda);
+	return distribution(generator);
+}
+
+int uniform_integral_gen(int min, int max)
+{
+	uniform_int_distribution<int> distribution(min, max);
+	return distribution(generator);
+}
+
+
+
+
+
+
+#endif
---
+++ src/scheduling_test.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include "../include/tasks.h"
+#include "../include/SchedTest/RMS.h"
+#include "../include/processors.h"
+#include "../include/random_gen.h"
+
+using namespace std;
+
+int main(int argc,char** argv)
+{
+	if(2 != argc)
+	{
+		cout<<"Usage: ./test [positive task number]"<<endl;
+		return 0;
+	}
+	
+	uint task_num = atoi(argv[1]);
+
+	if(0 >= task_num)
+	{
+		cout<<"Usage: ./test [positive task number]"<<endl;
+		return 0; 
+	}
+
+	//ProcessorSet processorset = ProcessorSet(processor_num);
+	TaskSet taskset = TaskSet();
+	RMS rms = RMS(1);//uniprocessor
+	
+	for(int i = 1; i <= task_num; i++)
+	{
+		int wcet = int(exponential_gen(3.5)*100);
+		int period = 1000;
+		taskset.add_task(wcet,period);	
+	}
+
+	for(int i = 0; i < task_num; i++)
+	{
+		cout << "Task " << i << ": Utilization: "<< taskset.get_task_utilization(i) << endl;
+	}
+	cout << fixed;
+	cout << "Total utilization:" << taskset.get_utilization_sum().get_d() << endl;
+	cout << "RM schedulability:" << (rms.is_RM_schedulable(taskset)) << endl;
+
+}
