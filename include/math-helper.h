9bd8b34f961f00bbc01e10e77665d854093dcdff
maolyang <maolyang@126.com>
2016-05-11 22:04:04 +0800

    shaping ceiling function

--- include/math-helper.h
+++ include/math-helper.h
@@ -11,21 +11,4 @@ static inline ulong ceiling(ulong numer, ulong denom)
 		return (numer / denom) + 1;
 }
 
-
-static inline int_t ceiling(const int_t &numer, const int_t &denom)
-{
-	int_t result;
-	mpz_cdiv_q(result.get_mpz_t(), numer.get_mpz_t(), denom.get_mpz_t());
-	return result;
-}
-
-
-static inline int_t round_up(const fraction_t &f)
-{
-	int_t result;
-	mpz_cdiv_q(result.get_mpz_t(), f.get_num_mpz_t(), f.get_den_mpz_t());
-	return result;
-}
-
-
 #endif
