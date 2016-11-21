db1040232a835fb883a16361f2760ceb906d1ccf
ChenZewei <czwking1991@gmail.com>
2016-11-21 22:28:13 +0800

    update new file.

---
+++ include/SchedTest/dag_gedf.h
@@ -0,0 +1,15 @@
+#ifndef DAG_GEDF_H
+#define DAG_GEDF_H
+
+#include <math.h>
+#include <math-helper.h>
+#include <algorithm>
+#include <iostream>
+
+#include "../tasks.h"
+#include "../processors.h"
+#include "../resources.h"
+
+
+
+#endif
