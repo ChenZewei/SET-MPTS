4434e5cc740495e95b794c774606306aa848a190
ChenZewei <czwking1991@gmail.com>
2016-12-21 09:39:23 +0800

    add file.

---
+++ include/SchedTest/blocking/lp/lp_dpcp.h
@@ -0,0 +1,28 @@
+#ifndef LP_DPCP_H
+#define LP_DPCP_H
+
+class Task;
+class TaskSet;
+class Resource;
+class ResourceSet;
+class ProcessorSet;
+class VarMapper;
+class LinearProgram;
+
+void lp_dpcp_objective(Task& ti, TaskSet& tasks, ResourceSet& resources, LinearProgram& lp, VarMapper& vars, LinearExpression *local_obj, LinearExpression *remote_obj);
+
+void lp_dpcp_local_objective(Task& ti, TaskSet& tasks, ResourceSet& resources, LinearProgram& lp, VarMapper& vars);
+
+void lp_dpcp_remote_objective(Task& ti, TaskSet& tasks, ResourceSet& resources, LinearProgram& lp, VarMapper& vars);
+
+//Constraint 1 [BrandenBurg 2013 RTAS] Xd(x,q,v) + Xi(x,q,v) + Xp(x,q,v) <= 1
+void lp_dpcp_constraint_1(Task& ti, TaskSet& tasks, ResourceSet& resources, LinearProgram& lp, VarMapper& vars);
+
+//Constraint 2 [BrandenBurg 2013 RTAS] for any remote resource lq and task tx except ti Xp(x,q,v) = 0
+void lp_dpcp_constraint_2(Task& ti, TaskSet& tasks, ResourceSet& resources, LinearProgram& lp, VarMapper& vars);
+
+//Constraint 3 [BrandenBurg 2013 RTAS]
+void lp_dpcp_constraint_3(Task& ti, TaskSet& tasks, ResourceSet& resources, LinearProgram& lp, VarMapper& vars);
+
+
+#endif
