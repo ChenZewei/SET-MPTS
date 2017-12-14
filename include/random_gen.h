0e5857500cb9ad2cb4470e5bcec94841787d753d
ZeweiChen <czwking1991@gmail.com>
2017-12-14 23:07:02 +0800

    --

--- include/random_gen.h
+++ include/random_gen.h
@@ -1,23 +1,25 @@
-#ifndef RANDOM_GEN_H
-#define RAMDOM_GEN_H
+/*
+    Copyright [2016] <Zewei Chen>
+*/
+#ifndef INCLUDE_RANDOM_GEN_H_
+#define INCLUDE_RANDOM_GEN_H_
 
+#include <types.h>
 #include <random>
 #include <chrono>
-#include "types.h"
-using namespace std;
 
-class Random_Gen
-{
-	private:
-		//static default_random_engine generator;
-		static mt19937 generator;
-	public:
-		//Random_Gen();
-		static double exponential_gen(double lambda);
-		static int uniform_integral_gen(int min, int max);
-		static ulong uniform_ulong_gen(ulong min, ulong max);
-		static double uniform_real_gen(double min, double max);
-		static bool probability(double prob);
+class Random_Gen {
+ private:
+    // static default_random_engine generator;
+    static mt19937 generator;
+
+ public:
+    // Random_Gen();
+    static double exponential_gen(double lambda);
+    static int uniform_integral_gen(int min, int max);
+    static ulong uniform_ulong_gen(ulong min, ulong max);
+    static double uniform_real_gen(double min, double max);
+    static bool probability(double prob);
 };
 
-#endif
+#endif  // INCLUDE_RANDOM_GEN_H_
--- src/random_gen.cpp
+++ src/random_gen.cpp
@@ -1,47 +1,45 @@
-#include "random_gen.h"
+/*
+    Copyright [2016] <Zewei Chen>
+*/
+#include <random_gen.h>
 #include <iostream>
 
-//default_random_engine Random_Gen::generator(std::chrono::system_clock::now().time_since_epoch().count());
-mt19937 Random_Gen::generator(std::chrono::system_clock::now().time_since_epoch().count());
+// default_random_engine
+//     Random_Gen::generator(std::chrono::system_clock::now().time_since_epoch().count());
+mt19937 Random_Gen::generator(std::chrono::system_clock::now()
+                              .time_since_epoch().count());
 
-double Random_Gen::exponential_gen(double lambda)
-{
-	exponential_distribution<double> distribution(lambda);
-	return distribution(generator);
+double Random_Gen::exponential_gen(double lambda) {
+    exponential_distribution<double> distribution(lambda);
+    return distribution(generator);
 }
 
-int Random_Gen::uniform_integral_gen(int min, int max)
-{
-	uniform_int_distribution<int> distribution(min, max);
-	return distribution(generator);
+int Random_Gen::uniform_integral_gen(int min, int max) {
+    uniform_int_distribution<int> distribution(min, max);
+    return distribution(generator);
 }
 
-ulong Random_Gen::uniform_ulong_gen(ulong min, ulong max)
-{
-	uniform_int_distribution<ulong> distribution(min, max);
-	return distribution(generator);
+ulong Random_Gen::uniform_ulong_gen(ulong min, ulong max) {
+    uniform_int_distribution<ulong> distribution(min, max);
+    return distribution(generator);
 }
 
-double Random_Gen::uniform_real_gen(double min, double max)
-{
-	uniform_real_distribution<double> distribution(min, max);
-	return distribution(generator);
+double Random_Gen::uniform_real_gen(double min, double max) {
+    uniform_real_distribution<double> distribution(min, max);
+    return distribution(generator);
 }
 
-bool Random_Gen::probability(double prob)
-{
-
-	int i = 1, j = 0;
-	if(1 < prob)
-		prob = 1;
-	while(prob != floor(prob) && j++ < 3)
-	{
-		prob *= 10;
-		i *= 10;
-	}
-	prob = floor(prob);
-	if(uniform_integral_gen(1, i) <= prob)
-		return true;
-	else 
-		return false;
+bool Random_Gen::probability(double prob) {
+    int i = 1, j = 0;
+    if (1 < prob)
+        prob = 1;
+    while (prob != floor(prob) && j++ < 3) {
+        prob *= 10;
+        i *= 10;
+    }
+    prob = floor(prob);
+    if (uniform_integral_gen(1, i) <= prob)
+        return true;
+    else
+        return false;
 }
